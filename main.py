71637df2c177246540f475dfe37fa01975835d62
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-01-08 22:30:41 +0300

    s

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-"""
-the recursive function signature would be like the following:
-    visited = []
-    def rec(currentLine, matrix)
-        if(EOF):
-            return
-        x = currentLine.split(" ")
-        originx = x[0]
-        originy = x[1]
-        serveRadius = x[2]
-        matrix = increment(matrix, originx, originy, serveRadius, serveRadius + 1, visited)
-        rec(theFollowingLine, matrix)
-    def increment(matrix, originx, originy, serveRadius, visited)
-        dijksstras algorithm?
-        if doesExist(visited, originx, originy) or serveRadius <= 0 or not isValid(matrix, originx, originy)
-            return
-        visited.append(""+originx+originy)
-        matrix[originx][originy] += 1
-        increment(matrix, originx + 1, originy, serveRadius - 1, visited)
-        increment(matrix, originx - 1, originy, serveRadius - 1, visited)
-        increment(matrix, originx, originy + 1, serveRadius - 1, visited)
-        increment(matrix, originx, originy - 1, serveRadius - 1, visited)
-        return matrix
-"""
 import sys
 
 
 def doesexist(existlist, originx, originy):
     for element in existlist:
-        if "" + originx + originy == element:
+        if str(originx) + str(originy) == element:
             return True
     return False
 
 
-def isvalid(matrixlen, x1, y):  # check if the provided x and y are valid in the matrix
-    if 0 <= x1 < matrixlen and 0 <= y < matrixlen:
+def isvalid(matrixlenght, x1, y):  # check if the provided x and y are valid in the matrix
+    if 0 <= x1 < matrixlenght and 0 <= y < matrixlenght:
         return True
     else:
         return False
@@ -53,34 +29,39 @@ def printmatrix(amatrix):
         print(" ".join(str(element) for element in row))
 
 
-def rec(currentline, matrix):
+def rec(currentline, matrix2):
     if currentline == '':
         return
-    x = currentline.split(" ")
-    originx = x[0]
-    originy = x[1]
-    serveradius = x[2]
+    temp = currentline.split(" ")
+    originx = matrixlen - int(temp[0])
+    originy = int(temp[1]) - 1
+    serveradius = int(temp[2]) + 1
     visited = []
-    matrix1 = increment(matrix, originx, originy, serveradius, visited)
+    visited.clear()
+    matrix1 = increment(matrix2, originx, originy, serveradius, visited)
+    printmatrix(matrix1)
     following_line = sys.stdin.readline()
     rec(following_line, matrix1)
 
 
-def increment(matrix, originx, originy, serveradius, visited):
-    if doesexist(visited, originx, originy) or serveradius <= 0 or not isvalid(matrix, originx, originy):
+def increment(matrixin, originx, originy, serveradius, visited):
+    if doesexist(visited, originx, originy) or serveradius <= 0 or not isvalid(matrixlen, originx, originy):
         return
-    matrix[originx][originy] += 1
-    increment(matrix, originx + 1, originy, serveradius - 1, visited)
-    increment(matrix, originx - 1, originy, serveradius - 1, visited)
-    increment(matrix, originx, originy + 1, serveradius - 1, visited)
-    increment(matrix, originx, originy - 1, serveradius - 1, visited)
+    matrixin[originx][originy] += 1
+    visited.append(str(originx) + str(originy))
+    increment(matrixin, originx + 1, originy, serveradius - 1, visited)
+    increment(matrixin, originx - 1, originy, serveradius - 1, visited)
+    increment(matrixin, originx, originy + 1, serveradius - 1, visited)
+    increment(matrixin, originx, originy - 1, serveradius - 1, visited)
     return matrix
+
 
 if __name__ == "__main__":
     firstline = sys.stdin.readline()
     x = firstline.split(" ")
-    matrixlen = x[0]
-    numofpizzarias = x[1]
+    matrixlen = int(x[0])
+    numofpizzarias = int(x[1])
     matrix = [[0 for j in range(int(matrixlen))] for i in range(int(matrixlen))]
-    rec(sys.stdin.readline(), matrix)
+    secondline = sys.stdin.readline()
+    rec(secondline, matrix)
     print(f"The maximum is {findmax(matrix)}")
